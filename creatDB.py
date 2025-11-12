a4914b89631c27edd319bc9886d92ac9b20142b5
M01096069 <fa3narshad@gmail.com>
2025-11-12 13:45:54 +0400

    Update: - New Function

--- creatDB.py
+++ creatDB.py
@@ -3,4 +3,7 @@ import sqlite3
 conn = sqlite3.connect('myTest.db')
 db0bject = conn.cursor()
 
-cursor.execute("""CREATE TABLE IF NOT EXISTS users (id inte""")
+cursor.execute("""CREATE TABLE IF NOT EXISTS users (id integer PRIMARY KEY AUTOINCREMENT, username text, password text)""")
+
+
+new_function = """INSERT INTO users (username, password) VALUES (?, ?)"""
